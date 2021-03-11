0fc62860de23e323f1cf360c239e63d3a162e14a
elykov <elykov.anatoly@outlook.com>
2021-03-11 20:53:04 +0200

    qwerty

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/index.js';
+import Footer from './components/Footer/index.js';
+import Layout from './components/Layout/index.js';
 
-function App() {
+import bg1 from './assets/backgrounds/background.jpg';
+
+console.log(bg1);
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, React JS!
-        </p>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Layout title={"Title 1"} descr={"Description 1"} urlBg={bg1} />
+      <Layout title={"Title 2"} descr={"Description 2"} colorBg={"green"} />
+      <Layout title={"Title 3"} descr={"Description 3"} urlBg={bg1} />
+      <Footer />
+    </>
   );
 }
 
